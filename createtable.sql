3d2736d887006f8f73ba263fb3c5468764d87b13
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2023-11-03 14:19:23 +0530

    Update createtable.sql

--- createtable.sql
+++ createtable.sql
@@ -19,6 +19,32 @@ go
 
 
 
+------
+
+  --table generated using IDE
+  CREATE TABLE [dbo].[flightCustomer](
+	[paymentid] [int] NOT NULL,
+	[custid] [numeric](3, 0) NULL,
+	[paymentmode] [varchar](50) NULL,
+	[paymentdate] [date] NULL,
+	[amt] [numeric](18, 0) NULL,
+ CONSTRAINT [PK_flightCustomer] PRIMARY KEY CLUSTERED 
+(
+	[paymentid] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[flightCustomer]  WITH CHECK ADD  CONSTRAINT [FK_flightCustomer_customer] FOREIGN KEY([custid])
+REFERENCES [dbo].[customer] ([custid])
+ON DELETE CASCADE
+GO
+
+  
+------
+
+  
+
 sp_help flightcustomer
 
 create table snacksInAir
